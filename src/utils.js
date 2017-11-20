fd46421156945f5ab72c59bd4359c6188e5f777b
Simon Jaspers <s.a.jaspers@gmail.com>
2017-11-20 21:22:46 +0100

    Add a map and filter util (untested)

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,48 @@
  * @return {string}
  */
 export const trim = str => str.trim();
+
+/**
+ * @param {*} x 
+ * @returns {boolean} 
+ */
+const isObject = x => Object.prototype.toString.call(x) === "[object Object]";
+
+/**
+ * Maps arrays or objects
+ * @param {function} f - a -> b
+ * @param {*} functor 
+ * @returns {*}
+ */
+export const map = (f, functor) => {
+  if (Array.isArray(functor) || typeof f.map === "function")
+    return functor.map(f);
+
+  if (isObject(functor))
+    return Object.entries(functor).reduce(
+      (o, [k, v]) => Object.assign(o, { [k]: f(v) }),
+      {}
+    );
+
+  return functor;
+};
+
+/**
+ * Filters arrays or objects.
+ * @param {function} pred - Filter function of x -> bool
+ * @param {*} filterable 
+ * @returns {*}
+ */
+export const filter = (pred, filterable) => {
+  if (Array.isArray(filterable) || typeof filterable.filter === "function")
+    return filterable.filter(pred);
+
+  if (isObject(filterable)) {
+    return Object.entries(filterable).reduce(
+      (o, [k, v]) => Object.assign(o, pred(v) ? { [k]: v } : {}),
+      {}
+    );
+  }
+
+  return filterable;
+};
