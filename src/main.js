b1cce0b9b29312ef6dcad59e28af884c8f7da4db
Simon Jaspers <s.a.jaspers@gmail.com>
2017-12-17 16:13:23 +0100

    Refactor crop and transform code

--- src/main.js
+++ src/main.js
@@ -1,57 +1,78 @@
 import { squareChanges } from "./imageHandling/squareChanges";
 
-const BoardImage = imgFile => {
+const pointReducer = (acc, { x, y }) => acc.concat(x, y);
+
+/**
+ * @param {[{x, y}]} refPoints
+ * @param {Image} img
+ * @returns {DataURL}
+ */
+const perspectiveTransform = (refPoints, img) => {
   const cvs = fx.canvas();
 
-  let transformFrom = [0, 256, 0, 0, 256, 0, 256, 256];
-  const transformTo = [0, 256, 0, 0, 256, 0, 256, 256];
+  const from = refPoints.reduce(pointReducer, []);
+  const to = [0, 256, 0, 0, 256, 0, 256, 256];
 
+  return cvs
+    .draw(cvs.texture(img))
+    .perspective(from, to)
+    .update()
+    .toDataURL();
+};
+
+/**
+ * @param {DataURL} dataURL
+ * @returns {ko.observable}
+ */
+const crop = (dataURL, writeTo) => {
+  const img = new Image();
+  const cvs = document.createElement("canvas");
+  cvs.width = 256;
+  cvs.height = 256;
+  const ctx = cvs.getContext("2d");
+
+  img.onload = () => {
+    ctx.drawImage(img, 0, 0);
+    writeTo(cvs.toDataURL());
+  };
+  img.src = dataURL;
+
+  return writeTo;
+};
+
+const BoardImage = imgFile => {
+  const transformFrom = ko.observableArray([
+    { x: 0, y: 256 },
+    { x: 0, y: 0 },
+    { x: 256, y: 0 },
+    { x: 256, y: 256 }
+  ]);
   const fourPoints = ko.observableArray([]);
+  // Write from points to transformFrom every 4th item
   fourPoints.subscribe(points => {
     if (points.length === 4) {
-      transformFrom = points.reduce((acc, { x, y }) => acc.concat(x, y), []);
-      redraw();
+      transformFrom(points);
       fourPoints([]);
     }
   });
 
-  // The returned image data url
-  const dataUrl = ko.observable("");
-
+  const myCrop = ko.observable();
   // Link a virtual img
   const img = new Image();
 
-  const redraw = () => {
-    const texture = cvs.texture(img);
-
-    // Draw & Transform
-    cvs
-      .draw(texture)
-      .perspective(transformFrom, transformTo)
-      .update();
-
-    // Crop
-    const sqImage = new Image();
-    sqImage.onload = () => {
-      const sqCvs = document.createElement("canvas");
-      sqCvs.width = sqCvs.height = 256;
-      const sqCtx = sqCvs.getContext("2d");
-      sqCtx.drawImage(sqImage, 0, 0);
-
-      // Write transformed image
-      dataUrl(sqCvs.toDataURL());
-    };
-
-    sqImage.src = cvs.toDataURL();
+  const redraw = transform => {
+    crop(perspectiveTransform(transform, img), myCrop);
   };
 
+  transformFrom.subscribe(redraw);
+
   // Load initial image
-  img.onload = redraw;
+  img.onload = () => redraw(transformFrom());
   img.src = URL.createObjectURL(imgFile);
 
   return {
     original: img.src,
-    crop: dataUrl,
+    crop: myCrop,
     onClick: (d, e) => {
       const bbox = e.target.getBoundingClientRect();
       fourPoints.push({
